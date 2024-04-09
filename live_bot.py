1adc48d7bba0d01f8003c174b2a5ebcd1181df6b
BuildTools <unconfigured@null.spigotmc.org>
2024-04-09 22:14:24 +0300

    Ну и ну! Теперь буду .env юзать

--- live_bot.py
+++ live_bot.py
@@ -1,2 +1,28 @@
-# 2024-04-09
-# https://www.twitch.tv/b1nc0d3z
+# #!/usr/bin/env python3.12
+# # -*- coding: utf-8 -*-
+#
+# # 2024-04-09
+# # https://www.twitch.tv/b1nc0d3z
+#
+# Future code Yandex.Practicum
+# AI-bot: Scenario generator
+# README.md for more
+#
+# FC Fil LiveCoding YaGPT
+# @fc_fil_lc_yagpt_bot
+# https://t.me/fc_fil_lc_yagpt_bot
+
+__version__ = '1.0'
+__author__ = 'Firip Yamagusi'
+
+import random
+from time import time_ns, time, strftime
+from random import choice
+
+import logging
+from telebot import TeleBot
+from telebot.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, Message
+
+import config
+
+TG_TOKEN = config.tg['token']
